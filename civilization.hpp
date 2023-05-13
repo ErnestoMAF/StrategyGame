6789a43e001edd7b053fb19686d8452a881b5515
ErnestoMAF <nemacflo@gmail.com>
2023-05-13 17:07:08 -0600

    [ADD] civilization hpp

---
+++ civilization.hpp
@@ -0,0 +1,51 @@
+#ifndef COMPUTADORA_HPP
+#define COMPUTADORA_HPP
+
+#include <string>
+
+using namespace std;
+
+class Civilization
+{
+    private:
+        string nombre;
+        int loc_x;
+        int loc_y;
+        double puntuacion;
+    
+    public:
+        Civilization() {}
+        Civilization(string& nombre,int loc_x,int loc_y, double puntuation) {
+            this->nombre = nombre;
+            this->loc_x = loc_x;
+            this->loc_y = loc_y;
+            this->puntuacion = puntuation;
+        }
+        
+        void setNombre(string& valor) {
+            nombre = valor;
+        }
+        string getNombre() {
+            return nombre;
+        }
+        void setLocX(int valor) {
+            loc_x = valor;
+        }
+        int getLocX() {
+            return loc_x;
+        }
+        void setLocY(int valor) {
+            loc_y = valor;
+        }
+        int getLocY() {
+            return loc_y;
+        }
+        void setPuntuacion(int valor) {
+            puntuacion = valor;
+        }
+        int getPuntuacion() {
+            return puntuacion;
+        }
+};
+
+#endif
